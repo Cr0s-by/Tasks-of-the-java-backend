413e827b425e669e0f52d38ea4ae88cae035c05b
Cr0sby <sarmishyur@mail.ru>
2025-03-31 22:07:07 +0300

    Delete Java_Backend/src/person directory
    delete person

--- Java_Backend/src/person/src/main/java/com/example/person/repository/CityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.person.repository;
-
-
-import com.example.person.model.City;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CityRepository extends JpaRepository<City, Long> {
-}
